9835933a4758b3652656f307f36784ed81d4d081
Abdelhadi Habchi <abdelhadi.deve@gmail.com>
2020-05-09 21:19:15 +0200

    Change questions name and edit design emoji question

--- screens/QuestionsScreen.js
+++ screens/QuestionsScreen.js
@@ -1,15 +1,15 @@
 import * as React from "react";
 import { View, Text, Button } from "react-native";
-import QuestionModel1 from "../components/QuestionModel1";
+import EmojiQuestionModel from "../components/EmojiQuestionModel";
 import QuestionModel2 from "../components/QuestionModel2";
 
 export default function QuestionsScreen(props) {
   const [questionState, setQuestionState] = React.useState(0);
 
   const allQuestionsComponent = [
-    <QuestionModel1 />,
+    <EmojiQuestionModel />,
     <QuestionModel2 />,
-    <QuestionModel1 />,
+    <EmojiQuestionModel />,
   ];
 
   return (
@@ -23,26 +23,33 @@ export default function QuestionsScreen(props) {
     >
       {allQuestionsComponent[questionState]}
 
-      <Button
-        title="Before"
-        onPress={() => {
-          console.log("clicked Before", questionState);
-          if (questionState === 0) return;
-          setQuestionState(() => {
-            return questionState - 1;
-          });
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
         }}
-      />
-      <Button
-        title="Next"
-        onPress={() => {
-          console.log("clicked Next");
-          if (questionState === allQuestionsComponent.length - 1) return;
-          setQuestionState(() => {
-            return questionState + 1;
-          });
-        }}
-      />
+      >
+        <Button
+          title="Before"
+          onPress={() => {
+            console.log("clicked Before", questionState);
+            if (questionState === 0) return;
+            setQuestionState(() => {
+              return questionState - 1;
+            });
+          }}
+        />
+        <Button
+          title="Next"
+          onPress={() => {
+            console.log("clicked Next");
+            if (questionState === allQuestionsComponent.length - 1) return;
+            setQuestionState(() => {
+              return questionState + 1;
+            });
+          }}
+        />
+      </View>
     </View>
   );
 }
