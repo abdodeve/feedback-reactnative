edbaef190d44d0a90dbcea411c3dff76eaa2f2e1
Abdelhadi Habchi <abdelhadi.deve@gmail.com>
2020-10-18 22:28:31 +0200

    Finish auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,12 @@
 import jwt_decode from "jwt-decode";
 import * as SecureStore from "expo-secure-store";
+import moment from "moment";
 
 /**
  * connect
  */
 export const connect = async () => {
-  // const res = await refreshToken();
-  console.log("++++++++++++ Connect - works ++++++++++++");
-  setAuthData({ idToken: "123", refreshToken: "456" });
-  const refreshToken = await SecureStore.getItemAsync("refreshToken");
-  console.log("refreshToken==>", refreshToken);
+  const res = await refreshToken();
 };
 
 export const refreshToken = async () => {
@@ -18,7 +15,13 @@ export const refreshToken = async () => {
     If Access token not exist that means
     this is the first http call so we should retrieve it first
   */
-  if (!idToken) {
+  try {
+    const decoded_idToken = jwt_decode(idToken);
+    if (!idToken || !decoded_idToken) {
+      getAccessToken();
+      return true;
+    }
+  } catch (error) {
     getAccessToken();
     return true;
   }
@@ -26,26 +29,30 @@ export const refreshToken = async () => {
   if (!(await isAccessTokenExpired())) {
     try {
       //Refresh the token
-      await refreshTheToken();
+      return await refreshTheToken();
     } catch (err) {
-      throw { error: "Cannot refresh token" };
-      // return false;
+      console.log(err);
+      return false;
     }
   }
-  return true;
+  return false;
 };
 
 const isAccessTokenExpired = async () => {
-  // const currentTime = new Date();
-  // const idToken = await SecureStore.getItemAsync("idToken");
+  try {
+    const currentTime = new Date();
+    const idToken = await SecureStore.getItemAsync("idToken");
 
-  // if (
-  //   moment(currentTime).add(10, 'm') <
-  //   moment(jwt_decode(idToken).exp * 1000)
-  // ) {
-  //   return true;
-  // }
-  return false;
+    if (
+      moment(currentTime).add(10, "m") < moment(jwt_decode(idToken).exp * 1000)
+    ) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
 
 const getAccessToken = async () => {
@@ -61,8 +68,10 @@ const getAccessToken = async () => {
       }
     );
     const resData = await response.json();
-    SecureStore.setItemAsync("idToken", resData.idToken);
-    SecureStore.setItemAsync("refreshToken", resData.refreshToken);
+    setAuthData({
+      idToken: resData.idToken,
+      refreshToken: resData.refreshToken,
+    });
   } catch (error) {
     console.log("getAccessToken:error====>", error);
   }
@@ -75,8 +84,8 @@ const refreshTheToken = async () => {
       grant_type: "refresh_token",
       refresh_token: refreshToken,
     };
-
     let formBody = [];
+
     for (let property in params) {
       let encodedKey = encodeURIComponent(property);
       let encodedValue = encodeURIComponent(params[property]);
@@ -96,15 +105,27 @@ const refreshTheToken = async () => {
       }
     );
     const resData = await response.json();
-    SecureStore.setItemAsync("idToken", resData.id_token);
-    SecureStore.setItemAsync("refreshToken", resData.refresh_token);
+    // console.log("resData====>", resData);
+    if (resData.error && resData.error.code === 400) return false;
+    setAuthData({
+      idToken: resData.id_token,
+      refreshToken: resData.refresh_token,
+    });
+    return true;
   } catch (error) {
     console.log("refreshTheToken:error====>", error);
+    return false;
   }
 };
 
 const setAuthData = (authData) => {
   for (let property in authData) {
     SecureStore.setItemAsync(property, authData[property]);
+  }
+};
+
+const resetAuthData = (authData) => {
+  for (let property in authData) {
+    SecureStore.deleteItemAsync(authData[property]);
   }
 };
